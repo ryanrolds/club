ff739a9d0c9b46431e662f5981226e8cd9edf28e
Chris Sjoblom <chris.sjoblom@gmail.com>
2020-05-15 09:07:12 -0700

    removed service worker from react app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core/styles'
 import App from './App';
 import theme from './theme';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
@@ -14,8 +13,3 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
