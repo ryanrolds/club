92c33584c41fe5c809224b5baa10abf1ad4a3925
Chris Sjoblom <csjoblom@users.noreply.github.com>
2020-05-17 09:03:45 -0700

    Add React App (#11)
    * base app is functioning
    
    * added local media setup to streamercardlist
    
    * removed service worker from react app
    
    * updated makefile for frontend
    
    * makefile changes
    
    * name update
    
    * added make file and docker configs for frontend
    
    * static readded

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { ThemeProvider } from '@material-ui/core/styles'
+import App from './App';
+import theme from './theme';
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstarts an elegent baseline to build on */}
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
