fa87d9a076540ed41804f42608bd85f47a5aace6
tsmada <adam@serouslabs.com>
2020-05-19 12:27:28 -0400

    feat: basic websocket test -- todo make work with SignalingServer impl

---
+++ frontend/src/helpers/signaling.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import WS from 'jest-websocket-mock'
+import SignalingServer from './signaling'
+
+describe('Messages', () => {
+  test('test: JOIN', async () => {
+    const server = new WS('ws://localhost:3000', { jsonProtocol: true })
+    //const client = new SignalingServer('ws://localhost:3000')
+    const client = new WebSocket('ws://localhost:3000')
+    await server.connected
+    server.send({ type: "GREETING", payload: "hello" });
+    client.send(JSON.stringify({ type: 'heartbeat', destId: 'server', payload: {} }))
+    console.log(client.readyState)
+    expect(client.readyState)
+  })
+})
