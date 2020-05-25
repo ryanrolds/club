84728cf95ec412e4d72daf28de6f1e91580defae
Chris Sjoblom <csjoblom@users.noreply.github.com>
2020-05-25 16:00:17 -0700

    Stand up React App (#25)
    * react app stood up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core/styles'
-import App from './App'
+import App from './views/app'
 import theme from './theme'
 
 ReactDOM.render(
