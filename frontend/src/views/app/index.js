25c73f7bf845af2f7eeca0c69fea3c7d47a2be4e
Chris Sjoblom <chris.sjoblom@gmail.com>
2020-05-24 21:30:39 -0700

    did some cleanup and prep

---
+++ frontend/src/views/app/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TopBar from '../../components/appBar/topBar'
+
+export default function App() {
+  return (
+    <>
+      <CssBaseline />
+      <main>
+        <TopBar />
+      </main>
+    </>
+  )
+}
