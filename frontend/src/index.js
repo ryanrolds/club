31884a74dec9593b90cfa4e872eceb91c0aca2a6
Chris Sjoblom <chris.sjoblom@gmail.com>
2020-05-29 22:43:58 -0700

    working rtc

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import { ThemeProvider } from '@material-ui/core/styles'
-import App from './views/app'
-import theme from './theme'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RTCMesh from './components/RTCMesh.jsx';
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstarts an elegent baseline to build on */}
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
-)
+let isHTTPS = window.location.protocol !== 'https:'
+const wsServer = (isHTTPS ? "ws" : "wss") + "://localhost:3001/room"
+ReactDOM.render(<RTCMesh URL={wsServer} />, document.getElementById('root'));
